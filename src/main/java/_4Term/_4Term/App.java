71152152ab35303e37fcfbc954c283a8c049f5a9
syd-baroya <sbaroya@calpoly.edu>
2018-02-28 10:38:12 -0800

    adding files for term

---
+++ src/main/java/_4Term/_4Term/App.java
@@ -0,0 +1,13 @@
+package _4Term._4Term;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
